27af28a94378b31b210bfe6c064ea10c4a716163
Alan Stewart <alankstewart@gmail.com>
2013-11-19 12:10:35 +1100

    Update AbstractSatin.java
    Used java.nio classes

--- src/main/java/alankstewart/satin/AbstractSatin.java
+++ src/main/java/alankstewart/satin/AbstractSatin.java
@@ -2,54 +2,64 @@ package alankstewart.satin;
 
 import alankstewart.satin.GaussianLaserBean.Gaussian;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.math.BigDecimal;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Formatter;
 import java.util.List;
 import java.util.Properties;
-import java.util.Scanner;
 import java.util.logging.Logger;
 
 import static java.lang.Float.parseFloat;
 import static java.lang.Integer.parseInt;
 import static java.math.BigDecimal.ROUND_HALF_UP;
 import static java.math.BigDecimal.valueOf;
+import static java.nio.charset.Charset.defaultCharset;
+import static java.nio.file.Files.createFile;
+import static java.nio.file.Files.deleteIfExists;
+import static java.nio.file.Files.readAllLines;
+import static java.nio.file.Paths.get;
 
 abstract class AbstractSatin {
 
     static final Logger LOGGER = Logger.getLogger(AbstractSatin.class.getName());
 
+    private static final String DATA_FILE_PATH = "dataFilePath";
+
+    private static Properties properties = new Properties();
+
     private enum CO2 {MD, PI}
+
+    static {
+        try (final InputStream inputStream = AbstractSatin.class.getResourceAsStream("/satin.properties")) {
+            properties.load(inputStream);
+        } catch (final IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     List<Integer> getInputPowers() throws IOException {
         final List<Integer> inputPowers = new ArrayList<>();
-        try (final InputStream inputStream = getInputStream("/pin.dat");
-             final Scanner scanner = new Scanner(inputStream)) {
-            while (scanner.hasNextLine()) {
-                inputPowers.add(parseInt(scanner.nextLine()));
-            }
-            return inputPowers;
+        for (final String line : readAllLines(get(properties
+                .getProperty(DATA_FILE_PATH), "pin.dat"), defaultCharset())) {
+            inputPowers.add(parseInt(line));
         }
+        return inputPowers;
     }
 
     List<Laser> getLaserData() throws IOException {
         final List<Laser> laserData = new ArrayList<>();
-        try (final InputStream inputStream = getInputStream("/laser.dat");
-             final Scanner scanner = new Scanner(inputStream)) {
-            while (scanner.hasNextLine()) {
-                final String[] gainMediumParams = scanner.nextLine().split("  ");
-                assert gainMediumParams.length == 4 : "The laser data record must have 4 parameters";
-                laserData
-                        .add(new Laser(gainMediumParams[0], parseFloat(gainMediumParams[1]),
-                                parseInt(gainMediumParams[2]
-                                .trim()), CO2.valueOf(gainMediumParams[3])));
-            }
-            return laserData;
+        for (final String line : readAllLines(get(properties
+                .getProperty(DATA_FILE_PATH), "laser.dat"), defaultCharset())) {
+            final String[] gainMediumParams = line.split("  ");
+            assert gainMediumParams.length == 4 : "The laser data record must have 4 parameters";
+            laserData.add(new Laser(gainMediumParams[0], parseFloat(gainMediumParams[1]), parseInt(gainMediumParams[2]
+                    .trim()), CO2.valueOf(gainMediumParams[3])));
         }
+        return laserData;
     }
 
     BigDecimal getElapsedTime(final long start, final long end) {
@@ -57,8 +67,9 @@ abstract class AbstractSatin {
     }
 
     void writeToFile(final Laser laser, final List<Gaussian> gaussianData) throws IOException {
-        final File outputFile = new File(getOutputFilePath(), laser.getOutputFile());
-        try (final Formatter formatter = new Formatter(outputFile)) {
+        final Path path = get(properties.getProperty("outputFilePath"), laser.getOutputFile());
+        deleteIfExists(path);
+        try (final Formatter formatter = new Formatter(createFile(path).toFile())) {
             formatter
                     .format("Start date: %s%n%nGaussian Beam%n%nPressure in Main Discharge = %skPa%nSmall-signal Gain = %s%% %nCO2 via %s%n%nPin\t\tPout\t\tSat. Int.\tln(Pout/Pin)\tPout-Pin%n(watts)\t\t(watts)\t\t(watts/cm2)\t\t\t(watts)%n", Calendar
                             .getInstance().getTime(), laser.getDischargePressure(), laser.getSmallSignalGain(), laser
@@ -70,18 +81,6 @@ abstract class AbstractSatin {
             }
             formatter.format("%nEnd date: %s%n", Calendar.getInstance().getTime());
         }
-    }
-
-    private InputStream getInputStream(final String name) {
-        return getClass().getResourceAsStream(name);
-    }
-
-    private String getOutputFilePath() throws IOException {
-        final Properties properties = new Properties();
-        try (final InputStream inputStream = getInputStream("/satin.properties")) {
-            properties.load(inputStream);
-        }
-        return properties.getProperty("outputFilePath");
     }
 
     static class Laser {
