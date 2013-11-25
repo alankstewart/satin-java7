19553aee9c555fab882af90ed01986424ceb6da4
Alan Stewart <alankstewart@gmail.com>
2013-11-25 12:31:57 +1100

    Update Satin.java
    Fix formatted string

--- src/main/java/alankstewart/satin/Satin.java
+++ src/main/java/alankstewart/satin/Satin.java
@@ -132,7 +132,7 @@ public final class Satin {
         int count = 0;
         try (final Formatter formatter = new Formatter(createFile(path).toFile())) {
             formatter
-                    .format("Start date: %s\n\nGaussian Beam\n\nPressure in Main Discharge = %skPa\nSmall-signal Gain = %s%% \nCO2 via %s\n\nPin\t\tPout\t\tSat. Int.\tln(Pout/Pin)\tPout-Pin\n(watts)\t\t(watts)\t\t(watts/cm2)\t\t\t(watts)\n", Calendar
+                    .format("Start date: %s\n\nGaussian Beam\n\nPressure in Main Discharge = %skPa\nSmall-signal Gain = %s\nCO2 via %s\n\nPin\t\tPout\t\tSat. Int.\tln(Pout/Pin)\tPout-Pin\n(watts)\t\t(watts)\t\t(watts/cm2)\t\t\t(watts)\n", Calendar
                             .getInstance().getTime(), laser.getDischargePressure(), laser.getSmallSignalGain(), laser
                             .getCarbonDioxide());
             for (final Integer inputPower : inputPowers) {
