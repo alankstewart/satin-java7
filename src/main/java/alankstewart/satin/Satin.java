e020fc48f9e1f0ed9c39f595ab9a05b62a5165a6
alanstewart <alankstewart@gmail.com>
2015-12-10 11:49:13 +1100

    Fix typo

--- src/main/java/alankstewart/satin/Satin.java
+++ src/main/java/alankstewart/satin/Satin.java
@@ -43,9 +43,9 @@ public final class Satin {
     private static final double W1 = 0.3;
     private static final double DR = 0.002;
     private static final double DZ = 0.04;
-    private static final double LAMDA = 0.0106;
+    private static final double LAMBDA = 0.0106;
     private static final double AREA = PI * RAD2;
-    private static final double Z1 = PI * pow(W1, 2) / LAMDA;
+    private static final double Z1 = PI * pow(W1, 2) / LAMBDA;
     private static final double Z12 = pow(Z1, 2);
     private static final double EXPR = 2 * PI * DR;
     private static final int INCR = 8001;
